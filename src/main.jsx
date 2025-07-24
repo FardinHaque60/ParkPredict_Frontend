a80fb9357cb1d832db6ac5fe51b36e10159b44c2
Fardin Haque <fardinhaque60@gmail.com>
2025-07-23 18:22:29 -0700

    changed initial files for global styles/entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import DataViz from './pages/data_viz/DataViz'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/data-viz" element={<DataViz />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 )
