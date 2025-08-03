cc9008eca0547f63f80c470136c646b07479bf78
Fardin Haque <fardinhaque60@gmail.com>
2025-08-02 19:02:52 -0700

    update initial layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
+// TODO add footer
 import './Home.css';
 
 function Home() {
     return (
         <div className="main-container">
-            {/* Meta tags and links should be placed in index.html or managed with react-helmet */}
+            {/* header */}
             <header className="sjsu-header u-bg--dark" role="banner">
                 <div className="wrap">
                     <a className="sjsu-title" href="https://www.sjsu.edu/" target="_blank" rel="noopener noreferrer">
@@ -12,59 +12,85 @@ function Home() {
                     </a>
                 </div>
             </header>
-
             <span className="sjsu-gradientbar"></span>
             <main className="sjsu-main"></main>
+            {/* main content */}
             <div className="wrap">
                 <h1 className="parking-title">
                     Parking Garage Fullness
                 </h1>
-                <h2 className="parking-services">
-                    <a href="https://www.sjsu.edu/parking/index.php" target="_blank" rel="noopener noreferrer">
-                        Parking Services
-                    </a>
-                </h2>
-                <p className="timestamp">
-                    Last updated 2025-2-14 6:28:00 AM <Link className="btn btn-primary" to="/GarageStatusPlain" style={{ width: "100%" }}> Refresh </Link>
-                </p>
+                {/* column headers */}
+                <div className="garages__header">
+                    <div>
+                        <h2 className="parking-services">
+                            <a href="https://www.sjsu.edu/parking/index.php" target="_blank" rel="noopener noreferrer">
+                                Parking Services
+                            </a>
+                            | 
+                            <a href="/data-viz">
+                                Stats for nerds
+                            </a>
+                        </h2>
+                    </div>
+                    <div className="columns">
+                        {/* TODO add refresh icon */}
+                        <p className="column__name">Fullness at <b>6:28 AM</b></p>
+                        {/* TODO convert to field and add tooltip */}
+                        <p className="column__name">Fullness at <b>9:28 AM</b></p>
+                    </div>
+                </div>
+                {/* start garages */}
                 <div className="garage">
                     <p></p>
                     <h2 className="garage__name">South Garage</h2>
-                    <p className="garage__text">
+                    <p className="garage__section">
                         <a className="garage__address" href="https://www.google.com/maps/place/377 S. 7th St., San Jose, CA 95112" target="_blank" rel="noopener noreferrer">
                             377 S. 7th St., San Jose, CA 95112
                         </a>
-                        <span className="garage__fullness">34 %</span>
+                        <span className="garage__stats">
+                            <span className="garage__percentage">34 %</span>
+                            <span className="garage__percentage">20 %</span>
+                        </span>
                     </p>
                     <p></p>
 
                     <h2 className="garage__name">West Garage</h2>
-                    <p className="garage__text">
+                    <p className="garage__section">
                         <a className="garage__address" href="https://www.google.com/maps/place/350 S. 4th St., San Jose, CA 95112" target="_blank" rel="noopener noreferrer">
                             350 S. 4th St., San Jose, CA 95112
                         </a>
-                        <span className="garage__fullness">13 %</span>
+                        <span className="garage__stats">
+                            <span className="garage__percentage">34 %</span>
+                            <span className="garage__percentage">20 %</span>
+                        </span>
                     </p>
                     <p></p>
 
                     <h2 className="garage__name">North Garage</h2>
-                    <p className="garage__text">
+                    <p className="garage__section">
                         <a className="garage__address" href="https://www.google.com/maps/place/65 S. 10th St., San Jose, CA 95112" target="_blank" rel="noopener noreferrer">
                             65 S. 10th St., San Jose, CA 95112
                         </a>
-                        <span className="garage__fullness">5 %</span>
+                        <span className="garage__stats">
+                            <span className="garage__percentage">34 %</span>
+                            <span className="garage__percentage">20 %</span>
+                        </span>
                     </p>
                     <p></p>
 
                     <h2 className="garage__name">South Campus Garage</h2>
-                    <p className="garage__text">
+                    <p className="garage__section">
                         <a className="garage__address" href="https://www.google.com/maps/place/1278 S. 10th Street, San Jose, CA 95112" target="_blank" rel="noopener noreferrer">
                             1278 S. 10th Street, San Jose, CA 95112
                         </a>
-                        <span className="garage__fullness">0 %</span>
+                        <span className="garage__stats">
+                            <span className="garage__percentage">34 %</span>
+                            <span className="garage__percentage">N/A</span>
+                        </span>
                     </p>
                     <p></p>
                 </div>
+                {/* end garages */}
                 <h2>Parking Shuttles</h2>
                 <p className="u-lift">
                     During Fall and Spring semesters, parking shuttles run regularly from parking lots to campus.
@@ -80,21 +106,6 @@ function Home() {
                     </a>
                 </p>
             </div>
-            <br />
-            <div className="row">
-                <div className="form-group"></div>
-            </div>
-            {/* Scripts and <body> tags are not used in React components */}
-            {/* Navbar and alert placeholders */}
-            <div className="container"></div>
-            <div className="navbar navbar-default">
-                <div className="container">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="/"></a>
-                    </div>
-                </div>
-            </div>
-            <div className="alert alert-dismissable"></div>
         </div>
     );
 }
