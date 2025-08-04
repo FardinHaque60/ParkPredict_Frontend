a9f7e9e70412092f11237e6dc15918963388aeb3
Fardin Haque <fardinhaque60@gmail.com>
2025-08-03 17:58:43 -0700

    service to make requests to api

---
+++ src/service/Api.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { DateTime } from 'luxon';
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// predict endpoints expect a date param
+export const quickPredictRequest = (timestamp) => {
+  return apiClient.get(`/quick_predict?timestamp=${timestamp}`);
+};
+
+export const predictRequest = (timestamp) => {
+  return apiClient.get(`/predict?timestamp=${timestamp}`);
+};
+
+export const allModelsPredictRequest = (timestamp) => {
+  return apiClient.get(`/predict_all?timestamp=${timestamp}`);
+}
+
+export const fetchPredictions = async (time, setPredictionTime, setPredictions, setIsLoading) => {
+  try {
+    const predictionTime = DateTime.fromFormat(time, 'HH:mm').setZone("America/Los_Angeles");
+    const response = await predictRequest(predictionTime.toISO());
+    setPredictionTime(predictionTime);
+    setPredictions(response.data.predictions);    
+    // console.log("Predictions:", response.data);  
+  } catch (err) {
+    console.error("Error fetching predictions:", err);
+    window.alert("Error fetching predictions. Please try again later.");
+  } finally {
+    setIsLoading(false);
+  }
+};
+
+export const fetchQuickPredictions = async (predictionTime, setPredictionTime, setPredictions, setIsLoading) => {
+  try {
+    const response = await quickPredictRequest(predictionTime.toISO());
+    setPredictionTime(predictionTime.plus({ minutes: 30 }));
+    setPredictions(response.data.predictions.next_30_mins);
+    // console.log("Quick Predictions:", response.data);
+  } catch (err) {
+    console.error("Error fetching quick predictions:", err);
+    window.alert("Error fetching predictions. Please try again later.");
+  } finally {
+    setIsLoading(false);
+  }
+};
---
+++ src/service/ResponseObjs.jsx
@@ -0,0 +1,12 @@
+// response object types from Api.jsx, keep consistent with backend response schemas
+export const QuickPredictObj = {
+    "next_30_mins": {},
+    "next_60_mins": {},
+    "next_120_mins": {},
+}
+
+export const PredictObj = {
+    "North Garage": "100",
+    "South Garage": "100",
+    "West Garage": "100",
+}
