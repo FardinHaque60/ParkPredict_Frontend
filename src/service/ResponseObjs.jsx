494f6ef015b395aaca08682f9b4c81b8f0e2f483
Fardin Haque <fardinhaque60@gmail.com>
2025-08-03 18:26:31 -0700

    remove unused obj

--- src/service/ResponseObjs.jsx
+++ src/service/ResponseObjs.jsx
@@ -1,10 +1,4 @@
 // response object types from Api.jsx, keep consistent with backend response schemas
-export const QuickPredictObj = {
-    "next_30_mins": {},
-    "next_60_mins": {},
-    "next_120_mins": {},
-}
-
 export const PredictObj = {
     "North Garage": "100",
     "South Garage": "100",
